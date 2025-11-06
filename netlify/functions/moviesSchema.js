0a8d7e86bb5f904e2aba489d7a3e0fdd20efdcc7
Enayatalibarkaat <inayatalibarkaat@gmail.com>
2025-11-06 17:41:15 +0530

    Create moviesSchema.js

---
+++ netlify/functions/moviesSchema.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const DownloadLinkSchema = new mongoose.Schema({
+  quality: String,
+  url: String,
+});
+
+const TelegramLinkSchema = new mongoose.Schema({
+  quality: String,
+  fileId: String,
+});
+
+const EpisodeSchema = new mongoose.Schema({
+  episodeNumber: Number,
+  title: String,
+  downloadLinks: [DownloadLinkSchema],
+  telegramLinks: [TelegramLinkSchema],
+});
+
+const SeasonSchema = new mongoose.Schema({
+  seasonNumber: Number,
+  episodes: [EpisodeSchema],
+});
+
+const GenreSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+});
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: String,
+    posterUrl: String,
+    backdropUrl: String,
+    description: String,
+    category: String,
+    actors: String,
+    director: String,
+    producer: String,
+    rating: Number,
+    downloadLinks: [DownloadLinkSchema],
+    telegramLinks: [TelegramLinkSchema],
+    seasons: [SeasonSchema],
+    trailerLink: String,
+    genres: [GenreSchema],
+    releaseDate: String,
+    runtime: Number,
+    tagline: String,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Movie || mongoose.model("Movie", MovieSchema);
