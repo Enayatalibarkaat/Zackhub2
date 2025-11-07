dbfab42b4207a5d2c062334be9c91d6fe34a4065
Enayatalibarkaat <inayatalibarkaat@gmail.com>
2025-11-07 09:10:07 +0530

    Update connect.js

--- netlify/functions/connect.js
+++ netlify/functions/connect.js
@@ -1,25 +1,16 @@
-```javascript
 import mongoose from "mongoose";
-let cached = global.mongoose;
-if (!cached) {
-cached = global.mongoose = { conn: null, promise: null };
-}
-export default async function connect() {
-if (cached.conn) {
-return cached.conn;
-}
-if (!cached.promise) {
+
 const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error("MONGODB_URI is missing!");
-Plain Text
-cached.promise = mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then((mongoose) => {
-  return mongoose.connection;
-});
-}
-cached.conn = await cached.promise;
-return cached.conn;
-}
-```
+
+export const connectDB = async () => {
+  if (!uri) throw new Error("MONGODB_URI is missing!");
+
+  if (mongoose.connection.readyState === 1) {
+    return;
+  }
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
