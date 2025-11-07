82dd285166a9837738865d3002b84ca18d854f4d
Enayatalibarkaat <inayatalibarkaat@gmail.com>
2025-11-07 10:48:08 +0530

    Update connect.js

--- netlify/functions/connect.js
+++ netlify/functions/connect.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
+let cached = global.mongoose;
 
-export const connectDB = async () => {
-  if (!uri) throw new Error("MONGODB_URI is missing!");
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
-  if (mongoose.connection.readyState === 1) {
-    return;
+export async function connect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI is missing!");
+
+    cached.promise = mongoose
+      .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose.connection);
   }
 
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
